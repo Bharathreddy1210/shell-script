cbcf939c1d88411576a4b424ba5015384930f871
Bharathreddy1210 <bharath1210.g@gmail.com>
2024-10-03 15:57:22 +0200

    first commit

--- 01-variables.sh
+++ 01-variables.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "Hello, print the content"
+#echo "Hello, print the content"
+
+PERSON1=Robert
+PERSON2=Mathew
+
+echo "$PERSON1:: Hello $PERSON2, Wass up?"
+echo "$PERSON2:: Nothing much, I'm planning to learn devops."
+echo "$PERSON1:: Ohh, thats good, with which cloud services ?"
+echo "$PERSON2:: with the AWS."
