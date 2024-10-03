646787fbe89dea9e967b4fc40423ae4d6f6c988b
Bharathreddy1210 <bharath1210.g@gmail.com>
2024-10-03 18:24:04 +0200

    variables

---
+++ 02-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Arguments / Parameters
+# $1,$2 
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, Wass up?"
+echo "$PERSON2:: Nothing much, I'm planning to learn devops."
+echo "$PERSON1:: Ohh, thats good, with which cloud services ?"
+echo "$PERSON2:: with the AWS."
