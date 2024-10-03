e3edfdf598c4e15f8cce22a0b5d0e40a2e367316
Bharathreddy1210 <bharath1210.g@gmail.com>
2024-10-03 18:35:50 +0200

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# "read" command and the encrpyted information.
+
+echo "Please enter the username: "
+
+read USERNAME
+
+echo "Enter the password : "
+
+read PASSWORD
+
+echo "login id is : $USERNAME, & password is : $PASSWORD"
