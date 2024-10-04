5b7653f8bea27b7a9e04d4b9588aae9d514ba676
Bharathreddy1210 <bharath1210.g@gmail.com>
2024-10-04 06:17:53 +0200

    special var

---
+++ 06-specialvar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "All variables:: $@"
+echo "Number of variables passed:: $#"
+echo "Script name:: $0"
+echo "current working directory:: $PWD"
+echo "Home directory of current user:: $HOME"
+echo "which user is running this script:: $USER"
+echo "Hostname:: $HOSTNAME"
+echo "Process ID of the current shell script:: $$"
