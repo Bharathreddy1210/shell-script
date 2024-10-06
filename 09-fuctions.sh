6482cc94787f4439d59f4ca0e1de778c7ed4826a
Bharathreddy1210 <bharath1210.g@gmail.com>
2024-10-06 07:36:03 +0200

    functions

--- 09-fuctions.sh
+++ 09-fuctions.sh
@@ -3,8 +3,13 @@
 USERID=$(id -u)
 
 VALIDATE(){
-   echo "Exit staus: $1"
-   echo "what are you doing: $2"
+   if [ $? -ne 0 ]
+   then 
+      echo "$2...FAILURE"
+      exit 1
+   else
+      echo "$2...SUCCESS"
+   fi
 }
 
 if [ $USERID -ne 0 ]
