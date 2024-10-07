ae1f34c0eca206ce427f6456113ac67f62d22198
Bharathreddy1210 <bharath1210.g@gmail.com>
2024-10-07 09:04:36 +0200

    packages-install

--- 11-packages-install.sh
+++ 11-packages-install.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE() {
+        if [ $? -ne 0 ] 
+      then 
+          echo "$2...FAILURE"
+          Exit 1
+      else
+          echo "$2...SUCCESS"
+        fi
+}
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then 
-   echo $R "please login with root user" $N
+   echo -e $R "please login with root user" $N
    exit 1
 else
-   echo $Y "Super User" $N
+   echo -e $Y "Super User" $N
 fi
 
 for i in $@
 do
   echo "package to install: $i"
+  dnf list installed $i &>>$LOGFILE
+  if [ $? eq 0 ]
+  then  
+      echo -e $Y "$i already installed....SKIP" $N
+  else
+      dnf install $i -y &>>$LOGFILE
+      VALIDATE $? "Installation of $i"
+  fi 
 done
+
 
 
